a45443a0f8d8f7bb96abc17ebe172c6eea4f5bcd
Ludw <ludwig-geyer@web.de>
2026-04-01 18:44:14 +0200

    add quadlet container

--- home/quadlet-user/default.nix
+++ home/quadlet-user/default.nix
@@ -49,6 +49,21 @@
     };
   };
 
+  virtualisation.quadlet.containers = {
+    echo-server = {
+      autoStart = true;
+      serviceConfig = {
+        RestartSec = "10";
+        Restart = "always";
+      };
+      containerConfig = {
+        image = "docker.io/mendhak/http-https-echo:31";
+        publishPorts = [ "127.0.0.1:8080:8080" ];
+        userns = "keep-id";
+      };
+    };
+  };
+
   # enable home-manager
   programs.home-manager.enable = true;
 
