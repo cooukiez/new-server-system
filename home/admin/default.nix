4d6fa3d9d32eb6a42acc96fce47caae64450bc1d
Ludw <ludwig-geyer@web.de>
2026-04-01 18:54:04 +0200

    remove container out of admin

--- home/admin/default.nix
+++ home/admin/default.nix
@@ -47,21 +47,6 @@
     };
   };
 
-  virtualisation.quadlet.containers = {
-    echo-server = {
-      autoStart = true;
-      serviceConfig = {
-        RestartSec = "10";
-        Restart = "always";
-      };
-      containerConfig = {
-        image = "docker.io/mendhak/http-https-echo:31";
-        publishPorts = [ "127.0.0.1:8080:8080" ];
-        userns = "keep-id";
-      };
-    };
-  };
-
   # enable home-manager
   programs.home-manager.enable = true;
 
