fd354b023b125a1193c9207a4ce2697a0e539589
home-server <ludwig-geyer@web.de>
2026-05-11 21:51:04 +0200

    updates

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -68,11 +68,6 @@ in
   # homepage
 
   # archiver
-  /*
-    "archiver/e_meili-key.age".publicKeys = allKeys;
-    "archiver/e_encrypt-key.age".publicKeys = allKeys;
-    "archiver/e_jwt-secret.age".publicKeys = allKeys;
-  */
   "archiver/e_admin-pass.age".publicKeys = allKeys;
 
   # ldap
