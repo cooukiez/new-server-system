8843b9f00b6ccdec8a919b87cd77723a5c248b0f
cooukiez <ludwig-geyer@web.de>
2026-07-08 18:53:16 +0200

    updates & formatting

--- modules/services/default.nix
+++ modules/services/default.nix
@@ -4,18 +4,12 @@ modules/services/default.nix
 part of server system
 created 2026-05-13 by ludw
 */
-{pkgs, ...}: let
-  migrate-db = pkgs.writeShellScriptBin "migrate-db" (builtins.readFile ./scripts/migrate-db.sh);
-in {
+{
   imports = [
     ./mail-archiver
     ./opengist
     ./papra
 
     ./metrics.nix
-  ];
-
-  environment.systemPackages = [
-    migrate-db
   ];
 }
